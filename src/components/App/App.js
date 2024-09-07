9781e59fbb3367110b1d9fc0a2992439d4835b7f
Bilal <bilalbelkebir9@gmail.com>
2024-09-07 06:17:54 +0100

    initialize the App component

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, {useState , useEffect} from "react";
+import Header from "../Header/Header";
+import Body from "../Body/Body";
+import "./App.css";
+
+function App() {
+    return (
+        <></>
+    )
+}
+
+export default App;
