92a716f95c179971ef2611b03f87b40d4957b373
Bilal <bilalbelkebir9@gmail.com>
2024-09-07 06:35:09 +0100

    create the first impression of Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import React , {useEffect , useState} from "react";
+import logo from "../images/logo512.png"
 import "./Header.css";
 
 function Header() {
     return (
-        <></>
+        <>
+            <header>
+                <div className="">
+                    <img src={logo} alt="react-logo" />
+                    <h1>ReactFacts</h1>
+                </div>
+                <div className="">
+                    <p>LIGHT</p>
+                    <div className="toggle">
+                        <div className="circle"></div>
+                    </div>
+                    <p>DARK</p>
+                </div>
+            </header>
+        </>
     )
 }
 
-export default Header;  
+export default Header;
